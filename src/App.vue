9d6934f82f4e57123adc2b63e85a57a642e5c07b
ajp84 <prater.anthony.j@gmail.com>
2023-03-07 17:14:43 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    Happy Birthday Jessica !!!
+  </header>
+  <HelloWorld />
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  background: linear-gradient(to right, #000 30%, #404 100% ) 50%, linear-gradient(90deg, #000 70%, #404 100%) 100%;
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #c0c;
+  text-align: center;
+  text-shadow: 1px 1px px #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Mystery Quest', cursive;
+  letter-spacing: 2px;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, Pagination, EffectCoverflow, Navigation, Zoom } from 'swiper';
+
+// Import Swiper Styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import "swiper/css/effect-coverflow";
+import 'swiper/css/navigation';
+import "swiper/css/zoom";
+
+const photos = [
+  'IMG_0044-3.JPG',
+  'IMG_0051.JPG',
+  'IMG_0054-3.JPG',
+  'IMG_0057-2.JPG',
+  'IMG_0059-3.JPG',
+  'IMG_0063.JPG',
+  'IMG_0066-2.JPG',
+  'IMG_0070-2.JPG',
+]
+
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true
+//   }
+// })
+</script>
+
+<template>
+<Swiper
+    :effect="'coverflow'"
+    :zoom="true"
+    :grabCursor="true"
+    :centeredSlides="true"
+    :slidesPerView="'auto'"
+    :coverflowEffect="{
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }"
+    :spaceBetween="30"
+    :autoplay="{
+      delay: 2500,
+      disableOnInteraction: false,
+    }"
+    :pagination="{
+      clickable: true,
+    }"
+    :navigation="true"
+    :modules="[Autoplay, Pagination, Navigation, EffectCoverflow, Zoom]"
+    class="mySwiper"
+  >
+    <SwiperSlide v-for="(photo, i) in photos" :key="i">
+      <img :src="`src/assets/${photo}`" alt="">
+    </SwiperSlide>
+  </Swiper>
+</template>
+
+<style scoped>
+
+
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.swiper-button-next,
+.swiper-button-prev {
+  color: #c0c;
+}
+
+@media (min-width: 1024px) {
+
+}
+</style>
